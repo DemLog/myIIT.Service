6da19404c2b8571d0704bf190a72856b4dabf0c6
DemLoveSky <dem.loginov@gmail.com>
2023-04-29 04:10:32 +0500

    Add controller for auth

--- src/modules/auth/auth.module.ts
+++ src/modules/auth/auth.module.ts
@@ -1,8 +1,24 @@
-import { Module } from '@nestjs/common';
-import { AuthService } from './auth.service';
-import { AuthController } from './auth.controller';
+import { Module } from "@nestjs/common";
+import { TypeOrmModule } from "@nestjs/typeorm";
+import { Auth } from "../../database/entities/auth.entity";
+import { Profile } from "../../database/entities/profile.entity";
+import { AuthService } from "./auth.service";
+import { AuthController } from "./auth.controller";
+import { JwtModule } from "@nestjs/jwt";
+import { ConfigModule, ConfigService } from "@nestjs/config";
 
 @Module({
+  imports: [
+    TypeOrmModule.forFeature([Auth, Profile]),
+    JwtModule.registerAsync({
+      imports: [ConfigModule],
+      useFactory: async (configService: ConfigService) => ({
+        secret: configService.get('JWT_SECRET'),
+        signOptions: { expiresIn: '1y' },
+      }),
+      inject: [ConfigService],
+    }),
+  ],
   providers: [AuthService],
   controllers: [AuthController],
 })
