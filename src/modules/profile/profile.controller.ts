f3580423c1339a63f7a96b6183c0c673402dddc4
DemLoveSky <dem.loginov@gmail.com>
2023-05-02 02:49:03 +0500

    Add profile.controller.ts

--- src/modules/profile/profile.controller.ts
+++ src/modules/profile/profile.controller.ts
@@ -1,4 +1,41 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Post, Put, Delete, Param, Body } from '@nestjs/common';
+import { ProfileService } from './profile.service';
+import { Profile } from '../../database/entities/profile.entity';
+import { CreateProfileDto } from './dto/create-profile.dto';
+import { UpdateProfileDto } from './dto/update-profile.dto';
+import { SearchProfileDto } from './dto/search-profile.dto';
 
-@Controller('profile')
-export class ProfileController {}
+@Controller('profiles')
+export class ProfileController {
+  constructor(private readonly profileService: ProfileService) {}
+
+  @Get()
+  async findAll(): Promise<Profile[]> {
+    return this.profileService.findAll();
+  }
+
+  @Get(':id')
+  async findOne(@Param('id') id: number): Promise<Profile> {
+    return this.profileService.findOne(id);
+  }
+
+  @Post()
+  async create(@Body() createProfileDto: CreateProfileDto): Promise<Profile> {
+    return this.profileService.create(createProfileDto);
+  }
+
+  @Put(':id')
+  async update(@Param('id') id: number, @Body() updateProfileDto: UpdateProfileDto): Promise<Profile> {
+    return this.profileService.update(id, updateProfileDto);
+  }
+
+  @Delete(':id')
+  async remove(@Param('id') id: number): Promise<void> {
+    return this.profileService.remove(id);
+  }
+
+  @Post('search')
+  async search(@Body() searchProfileDto: SearchProfileDto): Promise<Profile[]> {
+    return this.profileService.search(searchProfileDto);
+  }
+}
