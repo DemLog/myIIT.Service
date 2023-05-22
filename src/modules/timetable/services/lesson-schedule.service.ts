3548486b38fd1c05e18c4a99e2d3a93133785cb2
DemLoveSky <dem.loginov@gmail.com>
2023-05-23 02:08:13 +0500

    Edit lesson-schedule.service

--- src/modules/timetable/services/lesson-schedule.service.ts
+++ src/modules/timetable/services/lesson-schedule.service.ts
@@ -7,10 +7,7 @@ import { ResponseLessonScheduleDto } from "../dto/lessonSchedule/response-lesson
 import { LecturerTimetableService, SubjectTimetableService, TimeScheduleService } from "./";
 import { RoleService } from "../../role/role.service";
 import { UpdateLessonScheduleDto } from "../dto/lessonSchedule/update-lesson-schedule.dto";
-import { Subject } from "../../../database/entities/timetable/subject.entity";
-import { TimeSchedule } from "../../../database/entities/timetable/time-schedule.entity";
-import { Lecturer } from "../../../database/entities/timetable/lecturer.entity";
-import { Role } from "../../../database/entities/role.entity";
+import { DayWeek } from "../../../common/enums/dayWeek.enum";
 
 @Injectable()
 export class LessonScheduleService {
@@ -22,6 +19,12 @@ export class LessonScheduleService {
     private readonly timeScheduleService: TimeScheduleService,
     private readonly roleService: RoleService
   ) {
+  }
+
+  private isEvenWeek(date: Date): boolean {
+    const START_DAY = new Date(2021, 7, 30); // August 30, 2021
+    const numberOfWeeks = Math.floor((date.getTime() - START_DAY.getTime()) / (7 * 24 * 60 * 60 * 1000)) + 1;
+    return numberOfWeeks % 2 === 0;
   }
 
   async create(createLessonScheduleDto: CreateLessonScheduleDto): Promise<ResponseLessonScheduleDto> {
@@ -64,13 +67,59 @@ export class LessonScheduleService {
 
   async update(id: number, updateLessonScheduleDto: UpdateLessonScheduleDto): Promise<ResponseLessonScheduleDto> {
     const lessonSchedule = await this.getLessonSchedule(id);
-    let subject: Subject;
-    let timeSchedule: TimeSchedule;
-    let lecture: Lecturer[];
-    let groups: Role[];
 
     if (updateLessonScheduleDto.subject) {
-      
+      lessonSchedule.subject = await this.subjectService.getSubject(updateLessonScheduleDto.subject);
     }
+
+    if (updateLessonScheduleDto.time) {
+      lessonSchedule.time = await this.timeScheduleService.getTimeSchedule(updateLessonScheduleDto.time);
+    }
+
+    if (updateLessonScheduleDto.lecture) {
+      lessonSchedule.lecture = await Promise.all(updateLessonScheduleDto.lecture.map(id => this.lectureService.getLecturer(id)));
+    }
+
+    if (updateLessonScheduleDto.groups) {
+      lessonSchedule.groups = await Promise.all(updateLessonScheduleDto.groups.map(id => this.roleService.getRole(id)));
+    }
+
+    return this.lessonScheduleRepository.save(lessonSchedule);
+  }
+
+  async getTodayScheduleForGroup(userGroupId: number): Promise<ResponseLessonScheduleDto[]> {
+    const today = new Date().toLocaleDateString('en-US', { weekday: 'long' });
+    const isEvenWeek = this.isEvenWeek(new Date());
+    const group = await this.roleService.getRole(userGroupId);
+    const schedule = await this.lessonScheduleRepository.find({
+      where: {
+        groups: group,
+        dayWeek: DayWeek[today],
+        isEvenWeek,
+      },
+      relations: {
+        subject: true,
+        groups: true,
+        lecture: true,
+        time: true
+      },
+    });
+    return schedule;
+  }
+
+  async getWeeklyScheduleForGroup(userGroupId: number, isEvenWeek: boolean): Promise<ResponseLessonScheduleDto[]> {
+    const group = await this.roleService.getRole(userGroupId);
+    return await this.lessonScheduleRepository.find({
+      where: {
+        groups: group,
+        isEvenWeek,
+      },
+      relations: {
+        subject: true,
+        groups: true,
+        lecture: true,
+        time: true
+      },
+    });
   }
 }
